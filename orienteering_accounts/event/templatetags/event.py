d30ca63a6ac03e4e80ee0d88da60d702c6a67db8
Michal Kaspar <michal.kaspar@coex.cz>
2021-06-03 17:42:13 +0200

    fixed empty links

--- orienteering_accounts/event/templatetags/event.py
+++ orienteering_accounts/event/templatetags/event.py
@@ -8,6 +8,8 @@ from orienteering_accounts.event.models import Event
 
 @register.simple_tag
 def get_bulletin_url(event: Event):
+    if not event.links:
+        return None
     for _, link_dict in event.links.items():
         source_type = link_dict.get('SourceType')
         if source_type and source_type.get('ID') == settings.ORIS_SOURCE_TYPE_BULLETIN_ID:
