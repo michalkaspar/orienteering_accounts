0cdf45588be3008eaefbf35c964eff81f687bf3a
Michal Kašpar <kaspam13@gmail.com>
2026-01-29 20:10:04 +0100

    fixed date

--- orienteering_accounts/account/models.py
+++ orienteering_accounts/account/models.py
@@ -309,7 +309,7 @@ class Account(PermissionsMixin, AbstractBaseUser, BaseModel):
     @property
     def club_membership_variable_symbol(self):
         number = self.registration_number.replace('TZL', '')
-        return f'{str(date.year)}{number}'
+        return f'{datetime.now().year}{number}'
 
     @property
     def club_membership_payment_message(self):
